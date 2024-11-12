c31ea5713dc7d48217c4c9c78758c717b33ed2b7
Piotr Święcik <piotr.swiecik@gmail.com>
2024-11-12 11:58:57 +0100

    add langfuse tracing, add context

--- scripts/qdrant.py
+++ scripts/qdrant.py
@@ -5,11 +5,16 @@ from dotenv import load_dotenv
 from fastapi import Depends, FastAPI
 from pydantic import BaseModel
 import uvicorn
+from langfuse import Langfuse
+from langfuse.decorators import observe
 
 from aidevs3.services.ai_service import CompletionRequest, OpenAIService
 from aidevs3.services.vector_service import AsyncVectorService, VectorPointDto
 
 load_dotenv()
+
+
+langfuse = Langfuse(public_key=os.getenv("LANGFUSE_PUBLIC_KEY"), secret_key=os.getenv("LANGFUSE_SECRET_KEY"))
 
 
 class Message(BaseModel):
@@ -26,6 +31,7 @@ app = FastAPI()
 CONVERSATION_COLLECTION = "conversations"
 
 @app.post("/chat")
+@observe(name="qdrant_chat")
 async def chat(chat_request: ChatRequest):
     user_messages = filter(lambda m: m.role == "user", chat_request.messages)
 
@@ -40,11 +46,15 @@ async def chat(chat_request: ChatRequest):
     similar_messages = await vector_service.search(CONVERSATION_COLLECTION, last_message.content)
     print(f"found {len(similar_messages)} similar messages")
     for msg in similar_messages:
-        print(msg)
+        print(f"similar msg id: {msg.id}, score: {msg.score}, payload: {msg.payload}")
 
+    # similar messages are added to the conversation as context
+    context_messages = [Message(role=msg.payload["role"], content=msg.payload["text"]) for msg in similar_messages]
+
+    # add trace here
     answer = ai_service.completion(CompletionRequest(
         model="gpt-4o-mini",
-        messages=[{"role": last_message.role, "content": last_message.content}],
+        messages=[{"role": last_message.role, "content": last_message.content}, *context_messages],
     ))
 
     print("answer", answer)
